b90763261362e0939fd2b9ad0fd80b0872c91c77
Preeti Rana <preetirana2410@gmail.com>
2019-11-13 17:36:24 +0530

    NPM Scripts1

---
+++ js/scripts.js
@@ -0,0 +1,36 @@
+$(document).ready(function () {
+    $("#mycarousel").carousel({ interval: 1000 });
+    $("#carouselButton").click(function () {
+
+
+        if ($(carouselButton).children('span').hasClass('fa-pause')) {
+            $("#mycarousel").carousel('pause');
+            $(carouselButton).children('span').removeClass('fa-pause');
+            $(carouselButton).children('span').addClass('fa-play')
+
+        }
+        else if ($(carouselButton).children('span').hasClass('fa-play')) {
+            $("#mycarousel").carousel('cycle');
+            $(carouselButton).children('span').removeClass('fa-play');
+            $(carouselButton).children('span').addClass('fa-pause')
+
+        }
+    });
+    $("#loginButton").click(function () {
+        $("#loginModal").modal('show');
+    });
+    $(".loginClose").click(function () {
+        $("#loginModal").modal('hide');
+    });
+    $("#reserveForm").click(function () {
+        $("#reserveFormModal").modal('show');
+    });
+    $(".reserveFormClose").click(function () {
+
+        $("#reserveFormModal").modal('hide');
+    });
+    $('.modal').on('hidden.bs.modal', function () {
+    $(this).find('form').trigger('reset');
+})
+
+});
